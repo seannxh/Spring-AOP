d151c432ab10f119f27b4ac0835a171e1a77f1b8
seannxh <nohsean6@gmail.com>
2025-04-27 23:40:18 -0500

    Logger - Before - JoinPoint

--- src/main/java/com/springaop/springaop/aspect/LogginAspect.java
+++ src/main/java/com/springaop/springaop/aspect/LogginAspect.java
@@ -11,7 +11,10 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @Aspect
 public class LogginAspect {
-
+    //Weaving a framework that implements AOP AspectJ or Spring AOP
+    //Run Time when the pointcut is condition and a specific execution instance is called JoinPoint
+    // is the current place of package that my logic aka Advice is being applied to
+    //Before aspect - Method is called - execution(int com.springaop.springaop.business.BusinessService.calculateMax())
     private Logger logger = LoggerFactory.getLogger(getClass());
     //When?
     //execution(*PACKAGE.*.*)
@@ -19,6 +22,7 @@ public class LogginAspect {
     public void businessMethods(JoinPoint joinPoint) {
         logger.info("Before aspect - Method is called - {}", joinPoint);
         // empty — just a name for the pointcut
+        logger.info("Before Aspect - {} is called with - {}", joinPoint, joinPoint.getArgs());
     }
 
 
